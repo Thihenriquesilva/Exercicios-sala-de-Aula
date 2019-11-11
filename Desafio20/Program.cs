928d9c23eec0064d6dbb9c7a7d61a7e179b324d0
Thihenriquesilva <henriquesilva322@outlook.com>
2019-11-11 12:29:33 -0200

    alteração dos exercicios

---
+++ Desafio20/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Desafio20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Criar um programa que inicialize uma vetor de 10 inteiros com números aleatórios gerados com o método random e imprima duas listas, uma com os números pares outra com numeros impares.
+            
+            int result;
+            int [] vetor = new int [10];
+            List <int> par = new List<int>();
+            List <int> impar = new List<int>();
+
+            Random numeros = new Random();
+            
+
+            for(int i = 0; i< 10; i++){
+                vetor[i] = numeros.Next(0,1000);
+                result = vetor[i] %2;
+                if(result == 0){
+
+                    par.Add(vetor[i]);
+                }else{
+                    impar.Add(vetor[i]);
+                }
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+
+            foreach(var l in vetor){
+                System.Console.Write(l + "  ");
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+
+            foreach(var p in par){
+                System.Console.Write(p + "  ");
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine();
+
+            foreach(var im in impar){
+                System.Console.Write(im + "  ");
+            }
+        }
+    }
+}
---
+++ Desafio21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Desafio21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Criar um programa que simule o uso de um caixa eletronico, onde o usuário pode entrar um valor, e o programa calcula o número de notas a serem entregues pelo dispositivo. O dispositivo deve minimizar o número de notas entregues para o usuário.
+            int valor;
+            int ced = 50;
+            int totalced =0;
+
+            //50,20,10,5,2
+            System.Console.WriteLine( "==========================" );
+            System.Console.WriteLine( "Digite o valor do saque" );
+            valor =int.Parse(Console.ReadLine());
+            System.Console.WriteLine( "==========================" );
+
+            
+            while(true){
+
+                if(valor >= ced){   
+                    valor -=ced;
+                    totalced +=1;
+                    }
+                else if (valor <= ced){
+                        ced =20;
+                }
+                else if(valor == 0){
+                    break;
+                }
+                
+            }
+            
+        }
+    }
+}
